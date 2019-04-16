7fc69932c0a82dfc1599308139799d0980e4e200
PolBadWolf <gm.gameoff@gmail.com>
2019-04-16 12:58:53 +0500

    say Hello

---
+++ src/main/java/ru/polbadwolf/test/interfaces/remoteRMI/RemoteInterface.java
@@ -0,0 +1,23 @@
+package ru.polbadwolf.test.interfaces.remoteRMI;
+
+import java.io.Serializable;
+import java.rmi.*;
+
+public interface RemoteInterface extends Remote {
+    public String retStatus();
+    public class BlockData implements Serializable {
+        String[] name;
+        int[] content;
+
+        public BlockData() {
+            name = new String[0];
+            content = new int[0];
+        }
+
+        public BlockData(String[] name, int[] content) {
+            this.name = name;
+            this.content = content;
+        }
+
+    }
+}
