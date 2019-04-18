1cd79a584c6e4a7c61217fe53fa6b580d236ea86
PolBadWolf <gm.gameoff@gmail.com>
2019-04-18 11:34:07 +0500

    release

--- src/main/java/ru/polbadwolf/test/interfaces/remoteRMI/RemoteInterface.java
+++ src/main/java/ru/polbadwolf/test/interfaces/remoteRMI/RemoteInterface.java
@@ -2,22 +2,32 @@ package ru.polbadwolf.test.interfaces.remoteRMI;
 
 import java.io.Serializable;
 import java.rmi.*;
+import java.util.ArrayList;
 
 public interface RemoteInterface extends Remote {
-    public String retStatus();
+    public String retStatus() throws RemoteException;
+    public void putMessage(String string) throws RemoteException;
+    public void putData(BlockData data) throws RemoteException;
+    public void putData(BlockData[] data) throws RemoteException;
+
     public class BlockData implements Serializable {
-        public String[] name;
-        public int[] content;
+        public String name;
+        public int content;
 
         public BlockData() {
-            name = new String[0];
-            content = new int[0];
+            name = null;
+            content = 0;
         }
 
-        public BlockData(String[] name, int[] content) {
+        public BlockData(String name, int content) {
             this.name = name;
             this.content = content;
         }
 
+        @Override
+        public String toString() {
+            return name + ": \"" + Integer.toString(content) + "\"";
+        }
     }
+
 }
